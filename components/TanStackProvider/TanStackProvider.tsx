0cfc9b0c7a704ca6011d912a251bf8721fe5efa8
Iryna Dmytrenko <10i07r19a94@ukr.net>
2026-03-09 17:58:37 +0200

    finish hw09 auth

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,20 +1,58 @@
+// 'use client';
+
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { useState } from 'react';
+
+// type Props = {
+//   children: React.ReactNode;
+// };
+
+// export default function TanStackProvider({ children }: Props) {
+//   const [queryClient] = useState(() => new QueryClient());
+
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       {children}
+//       <ReactQueryDevtools />
+//     </QueryClientProvider>
+//   );
+// }
+
+// 'use client';
+
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+// const queryClient = new QueryClient();
+
+// export default function TanStackProvider({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       {children}
+//       {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
+//     </QueryClientProvider>
+//   );
+// }
+
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
 
-type Props = {
+const queryClient = new QueryClient();
+
+export default function TanStackProvider({
+  children,
+}: {
   children: React.ReactNode;
-};
-
-export default function TanStackProvider({ children }: Props) {
-  const [queryClient] = useState(() => new QueryClient());
-
+}) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
